dbbfeb6f14e6636a3fd44a2a5626ded929757047
svrohith9 <svrohith999@outlook.com>
2021-03-02 18:48:44 +0530

    Day1 basics

---
+++ Day1/Code.py
@@ -0,0 +1,33 @@
+# python print statement
+print("Harry Potter")
+
+
+print("---------------------------")
+print("Welcome to Day1")
+print("This is how we declare print statement")
+print("print('Hello World')")
+print("---------------------------")
+
+
+print("Hello"+"World")
+print("Hello World1 \nHello World2")
+
+# input function
+print("Hello " + input("Enter your Name: "))
+
+# len funtion
+print("Length of your name", len(input("Enter your Name again:")))
+
+# Switch data in variables
+a = input("Enter value of a: ")
+b = input("Enter value of b: ")
+
+print("initial value of a:", a)
+print("initial value of b:", b)
+
+temp = a
+a = b
+b = temp
+
+print("Switched value of a:", a)
+print("Switched value of b:", b)
---
+++ Day1/app.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+print("What's name of the city you grew up in?")
+city = input()
+print("What's the name of your pet?")
+pet = input()
+print("Your band name could be "+city+" "+pet)
