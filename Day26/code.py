16e0a2fd8601bebc05d5ba9e73299ff4a0b6f1a6
svrohith9 <svrohith999@outlook.com>
2021-03-27 00:30:39 +0530

    Update code.py
    lint and path of files updated

--- Day26/code.py
+++ Day26/code.py
@@ -1,6 +1,7 @@
 import random
+
 items = [1, 23, 4, 5]
-new_numbers = [i+1 for i in items]
+new_numbers = [i + 1 for i in items]
 print(new_numbers)
 
 
@@ -10,23 +11,23 @@ print(str_list)
 
 
 items = [1, 2, 4, 5]
-new_numbers = [i for i in items if (i+1) % 2 == 0]
+new_numbers = [i for i in items if (i + 1) % 2 == 0]
 print(new_numbers)
 
 print("***************************")
 
 nums = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squared_nums = [num*num for num in nums]
+squared_nums = [num * num for num in nums]
 print(squared_nums)
 
 # even nums
 print(f"{[num for num in nums if num%2==0]}")
 
 # intersection of file1 and file2 data
-with open(".\\Day26\\file1.txt") as file:
+with open(".\\100-days-python\\Day26\\file1.txt") as file:
     file1_data = [int(line) for line in file]
 
-with open(".\\Day26\\file2.txt") as file:
+with open(".\\100-days-python\\Day26\\file2.txt") as file:
     intersection_data = [int(line) for line in file if int(line) in file1_data]
     print(file.read())
 
@@ -40,8 +41,7 @@ names_scores = {name: random.randint(1, 100) for name in names}
 print(names_scores)
 
 
-passed_students = {key: value for key,
-                   value in names_scores.items() if value >= 50}
+passed_students = {key: value for key, value in names_scores.items() if value >= 50}
 print(passed_students)
 
 
