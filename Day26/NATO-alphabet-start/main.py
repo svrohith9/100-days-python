5b6c0b00e32086ddb8296492a3845a2f68e744b9
svrohith9 <svrohith999@outlook.com>
2021-03-27 23:19:20 +0530

    NATO alphabet

---
+++ Day26/NATO-alphabet-start/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = pd.read_csv(
+    "https://raw.githubusercontent.com/svrohith9/100-days-python/b7567b17a72663f7f056f3ffb0dcfe631af9edd1/Day26/NATO-alphabet-start/nato_phonetic_alphabet.csv"
+)
+user_str = input("Enter the String: ")
+output_str = [
+    row.code
+    for i in range(0, len(user_str))
+    for index, row in data.iterrows()
+    if row.letter.lower() == user_str[i]
+]
+print(output_str)
