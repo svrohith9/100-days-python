1ac11b861a5b9e5277e50e6d95f814659b22ee73
svrohith9 <svrohith999@outlook.com>
2021-03-27 23:23:49 +0530

    defaulting input to lowercase

--- Day26/NATO-alphabet-start/main.py
+++ Day26/NATO-alphabet-start/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = pd.read_csv(
     "https://raw.githubusercontent.com/svrohith9/100-days-python/b7567b17a72663f7f056f3ffb0dcfe631af9edd1/Day26/NATO-alphabet-start/nato_phonetic_alphabet.csv"
 )
-user_str = input("Enter the String: ")
+user_str = input("Enter the String: ").lower()
 output_str = [
     row.code
     for i in range(0, len(user_str))
