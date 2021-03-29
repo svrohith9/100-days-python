36b730699c93b9194d620ef93920ef5f94ebb590
svrohith9 <svrohith999@outlook.com>
2021-03-29 23:27:49 +0530

    pomodoro init

---
+++ Day28/pomodoro/main.py
@@ -0,0 +1,25 @@
+import tkinter
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tkinter.Tk()
+window.title("Pomodoro")
+
+canvas = tkinter.Canvas(width=200, height=300)
+# tkinter.PhotoImage(file=".\\100-days-python\\Day28\\pomodoro\\tomato.png")
+window.mainloop()
