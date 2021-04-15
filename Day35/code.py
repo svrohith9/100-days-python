87a0d778e08dcdb2cd5c27b77823b590bbba55ff
svrohith9 <svrohith999@outlook.com>
2021-04-16 01:10:24 +0530

    updated logic for umbrella requirement
    coords of Springfiled cause its raining right now

--- Day35/code.py
+++ Day35/code.py
@@ -2,13 +2,24 @@ import requests
 
 apikey = "b6af18726beeddb3570ceacf41e8e0ab"
 params = {
-    "lat": "17.385044",
-    "lon": "78.486671",
-    "appid": apikey
+    "lat": "37.215520",
+    "lon": "-93.292360",
+    "appid": apikey,
+    "exclude": "current,minutely,daily"
 }
 one_api_call = requests.get(
     url="https://api.openweathermap.org/data/2.5/onecall", params=params)
 
 one_api_call.raise_for_status()
 response = one_api_call.json()
-print(response)
+
+weather_ids = [x['weather'][0]["id"] for x in response["hourly"]]
+is_umbrella_required = False
+for id in weather_ids:
+    if int(id) < 700:
+        is_umbrella_required = True
+
+if is_umbrella_required:
+    print("Bring Umbrella !")
+else:
+    print("Umbrealla not requried")
