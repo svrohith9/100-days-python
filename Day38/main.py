77a4ee9ebdc6a842cd1de5269cc452b336706dfb
svrohith9 <svrohith999@outlook.com>
2021-05-02 14:58:37 +0530

    init - Intelligent exercise tracking app

---
+++ Day38/main.py
@@ -0,0 +1,31 @@
+import requests
+
+APP_ID =   # "YOUR APP ID"
+API_KEY =   # "YOUR API_KEY"
+
+GENDER =   # YOUR GENDER
+WEIGHT_KG =   # YOUR WEIGHT
+HEIGHT_CM =   # YOUR HEIGHT
+AGE =   # YOUR AGE
+
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
