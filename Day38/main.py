a121a6bd48d116df40977c75a7aa973e23f0a9fb
svrohith9 <svrohith999@outlook.com>
2021-05-02 16:31:48 +0530

    basic auth sheety and post to Gsheets

--- Day38/main.py
+++ Day38/main.py
@@ -1,13 +1,18 @@
 import requests
+from datetime import datetime
+import os
+
+GENDER = "male"
+WEIGHT_KG = "81"
+HEIGHT_CM = "171"
+AGE = "23"
 
 APP_ID =   # "YOUR APP ID"
 API_KEY =   # "YOUR API_KEY"
-
 GENDER =   # YOUR GENDER
 WEIGHT_KG =   # YOUR WEIGHT
 HEIGHT_CM =   # YOUR HEIGHT
 AGE =   # YOUR AGE
-
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -29,3 +34,40 @@ parameters = {
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 print(result)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    # Basic Auth
+    sheet_response = requests.post(
+        SHEET_ENDPOINT,
+        json=sheet_inputs,
+        auth=(
+            USERNAME,
+            PASSWORD
+        )
+    )
+
+    # Bearer Token
+    # bearer_headers = {
+    #     "Authorization": f"Bearer {TOKEN}",
+    #     "Content-Type": "application/json",
+    # }
+    # sheet_response = requests.post(
+    #     SHEET_ENDPOINT,
+    #     json=sheet_inputs,
+    #     headers=bearer_headers
+    # )
+
+    print(sheet_response.text)
