a955810a05e5528ada78c46596137f1f33827354
svrohith9 <svrohith999@outlook.com>
2021-05-06 02:59:39 +0530

    Email integration and multi user alert

--- Day39/flight-search/notification_manager.py
+++ Day39/flight-search/notification_manager.py
@@ -1,9 +1,13 @@
+import smtplib
 from twilio.rest import Client
 
 TWILIO_SID =  # YOUR TWILIO ACCOUNT SID
 TWILIO_AUTH_TOKEN =  # YOUR TWILIO AUTH TOKEN
 TWILIO_VIRTUAL_NUMBER =  # YOUR TWILIO VIRTUAL NUMBER
 TWILIO_VERIFIED_NUMBER =  # YOUR TWILIO VERIFIED NUMBER
+MAIL_PROVIDER_SMTP_ADDRESS =  # YOUR EMAIL PROVIDER SMTP ADDRESS "smtp.gmail.com"
+MY_EMAIL =  # YOUR EMAIL
+MY_PASSWORD =  # YOUR PASSWORD
 
 
 class NotificationManager:
@@ -17,3 +21,16 @@ class NotificationManager:
             from_=TWILIO_VIRTUAL_NUMBER,
             to=TWILIO_VERIFIED_NUMBER,
         )
+        print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode(
+                        'utf-8')
+                )
