8e98d0d81c62b8770efc0fa14d8c10ded7fe64d1
svrohith9 <svrohith999@outlook.com>
2021-05-25 20:02:01 +0530

    added wrapper for bold, underline

--- Day54/main.py
+++ Day54/main.py
@@ -2,12 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return f'<b>{function()}</b>'
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return f'<u>{function()}</u>'
+    return wrapper
+
+
 @app.route('/')
+@make_underlined
 def hello_world():
     return "Hello"
 
 
 @app.route('/bye')
+@make_underlined
 def bye():
     return "Bye"
 
