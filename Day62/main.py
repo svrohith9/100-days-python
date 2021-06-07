a65536ccf483699f1c5be5f1d98b4fccccff9ab2
svrohith9 <svrohith999@outlook.com>
2021-06-08 02:55:21 +0530

    add data WTForm

--- Day62/main.py
+++ Day62/main.py
@@ -1,22 +1,47 @@
-from flask.app import Flask
+from flask import Flask, redirect
 from flask.templating import render_template
 from wtforms.validators import DataRequired, URL
-from wtforms.fields.core import StringField
+from wtforms.fields.core import RadioField, SelectField, StringField
+from flask.helpers import url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import SubmitField
+import flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any-secret-string'
+Bootstrap(app)
 
 
-class Data:
+class Data(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    url = StringField('Geo-Link', validators=[DataRequired(), URL()])
-    food = StringField('Food 🍕', validators=[DataRequired()])
-    spot = StringField('Spot 🏖️', validators=[DataRequired()])
-    price = StringField('Price 💰', validators=[DataRequired()])
+    url = StringField(
+        'Geo-Link', validators=[DataRequired(), URL()])
+    food_rating = SelectField('Food 🍕', choices=[
+        '🍕', '🍕🍕', '🍕🍕🍕', '🍕🍕🍕🍕', '🍕🍕🍕🍕🍕'], validators=[DataRequired(message="Please Choose rating")])
+    spot_rating = SelectField('Spot 🏖️', choices=[
+        '🏖️', '🏖️🏖️', '🏖️🏖️🏖️', '🏖️🏖️🏖️🏖️', '🏖️🏖️🏖️🏖️🏖️'], validators=[DataRequired(message="Please Choose rating")])
+    price_rating = RadioField('Price 💰', choices=[
+        '💰', '💰💰', '💰💰💰', '💰💰💰💰', '💰💰💰💰💰'], validators=[DataRequired(message="Please Choose rating")], validate_choice=True)
+    submit = SubmitField("submit")
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add():
+    data = Data()
+    message = ""
+    if flask.request.method == 'POST':
+        if data.validate_on_submit():
+            print(data.name.data)
+            message = "Sucessfully added"
+        else:
+            message = "failed to add data"
+    return render_template('add_data.html', form=data, message=message)
 
 
 if __name__ == "__main__":
